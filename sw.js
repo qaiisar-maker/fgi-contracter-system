c77e373158fbae7a0076af81c2e0ac0f0e06c6a8
qaiisar-maker <qaiisar@gmail.com>
2026-05-02 17:23:46 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "fgi-contractor-v2";
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.status === 200) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match(e.request)))
+  );
+});
